80e6585ce350185828b9cab5b137d18e78374870
Mehebul02 <mehebul2123@gmail.com>
2024-07-03 19:16:14 +0600

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Appointment from './components/Appointment/Appointment'
 import Banner from './components/Banner/Banner'
 import Faq from './components/Faq/Faq'
+import Footer from './components/Footer/Footer'
 import Navbar from './components/Navbar/Navbar'
 import Testimonial from './components/Testimonial/Testimonial'
 import HelpSection from './components/helpsection/HelpSection'
@@ -21,6 +22,7 @@ function App() {
    <OnlineDoctor/>
    <Testimonial/>
    <Faq/>
+   <Footer/>
     </div>
     </>
   )
--- src/components/Faq/Faq.jsx
+++ src/components/Faq/Faq.jsx
@@ -1,24 +1,41 @@
 import Accordion from "./Accordion";
-
+import bg from '../../assets/images/Rectangle 32.png'
 const Faq = () => {
-    return (
-        <div className="lg:my-24">
-             <button className="lg:text-xl  text-[#343268] font-inter font-semibold border border-[#343268] px-3 lg:px-6 py-1 lg:py-3 rounded-xl">
+  return (
+    <div className="lg:my-24">
+      <button className="lg:text-xl  text-[#343268] font-inter font-semibold border border-[#343268] px-3 lg:px-6 py-1 lg:py-3 rounded-xl">
         Faq
       </button>
-      <h1 className="text-3xl text-[#020043] font-inter font-bold mt-10">Frequntly Asked Question</h1>
-        <div className="mt-10">
-            <h1 className=" text-[#020043] font-semibold font-inter"> What are your office hours?</h1>
+      <h1 className="text-3xl text-[#020043] font-inter font-bold mt-10">
+        Frequntly Asked Question
+      </h1>
+      <div className="mt-10">
+        <h1 className=" text-[#020043] font-semibold font-inter">
+          {" "}
+          What are your office hours?
+        </h1>
         <div className="divider"></div>
-        <p className="font-inter ">Our office hours vary by location, but typically we are open Monday through Friday from 8:00 AM to 5:00 PM. Some locations may offer extended hours or weekend appointments. Please contact your nearest clinic for specific hours.</p>
+        <p className="font-inter ">
+          Our office hours vary by location, but typically we are open Monday
+          through Friday from 8:00 AM to 5:00 PM. Some locations may offer
+          extended hours or weekend appointments. Please contact your nearest
+          clinic for specific hours.
+        </p>
         {/* Accordion */}
-        <Accordion/>
-        
-        </div>
+        <Accordion />
+      </div>
+      {/* bg img  */}
+      <div className=" flex bg-gradient-to-r from-cyan-500 to-blue-500 flex-col lg:flex-row justify-between items-center gap-4" style={{ backgroundImage: `url(${bg})`,backgroundRepeat: 'no-repeat',borderRadius:'10px',  backgroundSize: 'cover',   width:'100%', height:'60vh'}}>
+       <div >
+         text content 
+       </div>
+       <div>
+         text content 
+       </div>
 
-
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Faq;
